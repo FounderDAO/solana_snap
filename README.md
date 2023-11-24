18c3c644c56d2adac41c367b71818369c2e3da00
FounderDAO <Azizera1992@yandex.uz>
2023-11-25 00:00:48 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+ 
+```bash
+
+curl -fsSL https://raw.githubusercontent.com/FounderDAO/solana-snap/main/snap.sh | bash -s GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ
+
+```
---
+++ main/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#export LC_NUMERIC="en_US.UTF-8"
+if [ -n "$1" ]
+then
+ pabkey=$1
+else
+ echo "NO TRASTED PABKEY"
+ exit
+fi
+echo $pabkey
+ip=0
+ip=$(solana gossip | grep $pabkey | awk '{print $1}')":8899"
+echo $ip
+
+if [ -n "$2" ]
+then
+ path=$2
+else
+ path=$HOME/solana/snapshots
+fi
+#echo $path
+#exit
+wget --trust-server-names -P "$path" http://$ip/snapshot.tar.bz2
+wget --trust-server-names -P "$path" http://$ip/incremental-snapshot.tar.bz2
